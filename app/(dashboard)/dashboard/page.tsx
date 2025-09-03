e50856d06c68c09b84b45d7baf5692bec12c45f2
kartikeyevanik <kartikey.evanik@gmail.com>
2025-09-03 11:09:47 +0530

    v1.0.6

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Page() {
     return (
         <div>
             <h1>Main dashboard</h1>
+            <Link href={"dashboard/audit-logs"} className="text-blue-400">Go to audit logs</Link>
         </div>
     )
 }
